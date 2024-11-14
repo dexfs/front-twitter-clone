554ebd4582a2957c1e1cfd3bf258f896a451ca6c
André Santos <andre@dexdev.com.br>
2024-11-14 09:07:55 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { GlobalStyle } from './styles/global.ts';
+
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <h1>Vite + React</h1>
+    </>
+  )
+}
+
+export default App;
